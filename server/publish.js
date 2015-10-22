ccd2f1331dbd545d62312d1c0ebf6b5a985f2cab
Rajesh Danala <rajesh.danala@gmail.com>
2015-10-22 03:21:29 -0400

    -- broken background image -- broken listing details frontend

--- server/publish.js
+++ server/publish.js
@@ -1,9 +1,12 @@
  Meteor.publish("grandviewpages", function(){
-   return GrandviewPages.find();
+   return GrandviewPages.find({});
  });
 
  Meteor.publish("grandviewlistings", function(){
    return GrandviewListings.find();
+ });
+ Meteor.publish("grandviewlistingsWithOptions", function(options){
+   return GrandviewListings.find({},options);
  });
  // Meteor.publish("findGrandviewPageBySeourl", function(menu){
  //   return GrandviewPages.findOne({seourl:menu});
